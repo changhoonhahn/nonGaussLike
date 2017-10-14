03f5049b23f8a36079f0f699018ff004810ac283
ChangHoon Hahn <chh327@nyu.edu>
2017-10-13 18:24:37 -0700

    Model now working; mcmc implementation required

--- nongausslike/infer.py
+++ nongausslike/infer.py
@@ -7,7 +7,7 @@ import model as Mod
 
 
 def data(ell, zbin, nors): 
-    ''' Read in P(k) measurements of data 
+    ''' Read in P(k) measurements of BOSS from Florian  
     '''
     if ell == 0:
         str_pole = 'mono'
@@ -18,10 +18,157 @@ def data(ell, zbin, nors):
     str_pole += 'pole'
 
     fname = ''.join([UT.dat_dir(), 'Beutler/public_material_RSD/',
-        'Beutleretal_pk_', str_pole, '_DR12_', nors.upper(), '_z', str(zbin), '_prerecon_120.dat'])
-
+        'Beutleretal_pk_', str_pole, '_DR12_', nors.upper(), '_z', str(zbin), 
+        '_prerecon_120.dat'])
     k_central, k_mean, pk = np.loadtxt(fname, skiprows=31, unpack=True, usecols=[0,1,2])
     return k_central, pk 
+
+
+def lnPrior(theta):
+    ''' log prior function -- ln(Prior(theta)). theta should have 
+    [alpha_perp, alpha_para, fsig8, 
+    b1NGCsig8, b1SGCsig8, b2NGCsig8, b2SGCsig8, NNGC, NSGC, sigmavNGC, sigmavSGC]
+    '''
+    if len(theta) != 11: 
+        raise ValueError("theta should have 11 parameters")
+
+    #prior_min = [0.5, 0.5, 0.1, 0.3, 0.3, -6., -6., -10000., -10000., 0.5, 0.5]
+    #prior_max = [1.5, 1.5, 1.1, 5., 5., 6., 6., 10000., 10000., 15., 15.]
+    prior_min = [0.8, 0.8, 0.1, 0.3, 0.3, -6., -6., -10000., -10000., 0.5, 0.5]
+    prior_max = [1.4, 1.4, 1.1, 5., 5., 6., 6., 10000., 10000., 15., 15.]
+
+    for i in range(len(theta)): 
+        if prior_min[i] < theta[i] < prior_max[i]: 
+            pass
+        else: 
+            return -np.inf 
+    return 0. 
+
+
+def lnLike(theta, k_list, pk_ngc_list, pk_sgc_list, Cinv_ngc, Cinv_sgc):
+    ''' log likelihood function 
+
+    Parameters
+    ----------
+    theta : array
+        cosmological parameters : alpha_perp, alpha_para, fsig8 and 
+        nuisance parameters : b1NGCsig8, b1SGCsig8, b2NGCsig8, b2SGCsig8, 
+        NNGC, NSGC, sigmavNGC, sigmavSGC
+
+    k_list : list
+        list of k values for the mono, quadru, and hexadecaopoles -- [k0, k2, k4]
+
+    pk_ngc_list : list 
+        
+
+    '''
+    if theta[0] > 0.8 and theta[0] < 1.4 and theta[1] > 0.8 and theta[1] < 1.4:
+        binrange1, binrange2, binrange3 = len(k_list[0]), len(k_list[1]), len(k_list[2])
+        maxbin1 = len(k_list[0])+1
+
+        k = np.concatenate(k_list)
+        pk_ngc = np.concatenate(pk_ngc_list)
+        pk_sgc = np.concatenate(pk_sgc_list)
+
+        modelX = Mod.taruya_model(100, binrange1, binrange2, binrange3, maxbin1, 
+                k, theta[0], theta[1], theta[2], theta[3], 
+                theta[4], theta[5], theta[6], theta[7], 
+                theta[8], theta[9], theta[10])
+    
+        model_ngc = modelX[0]
+        model_sgc = modelX[1]
+        #print("dummy_model_NGC = ", dummy_model_NGC, len(x))
+        #for i in range(0, 37):
+        #    print("k = ", x[i], dummy_model_NGC[i], dummy_model_SGC[i])
+
+        diff_ngc = model_ngc - pk_ngc
+        diff_sgc = model_sgc - pk_sgc 
+        chi2_ngc = np.dot(diff_ngc, np.dot(Cinv_ngc, diff_ngc))
+        chi2_sgc = np.dot(diff_sgc, np.dot(Cinv_sgc, diff_sgc))
+        return -0.5*(chi2_ngc + chi2_sgc)
+    else: 
+        return -0.5*(10000.)
+
+
+def lnPost(theta, k_list, pk_ngc_list, pk_sgc_list, Cinv_ngc, Cinv_sgc):
+    ''' log posterior 
+    '''
+    lp = lnPrior(theta)
+    if np.isfinite(lp):
+        return lp + lnlike(theta, k_list, pk_ngc_list, pk_sgc_list, Cinv_ngc, Cinv_sgc)
+    else:
+        return -np.inf
+
+
+def mcmc(zbin=1, nwalkers=48, Nchains=4): 
+    '''
+    '''
+    # read in BOSS P(k) 
+
+    pos=[]
+    sampler=[]
+    rstate = np.random.get_state()
+    temperature = 2.e-3
+    
+    if zbin == 1: # 0.2 < z < 0.5 
+        start = [1.008, 1.001, 0.478, 1.339, 1.337, 1.16, 0.32, -1580., -930., 6.1, 6.8] 
+
+    print("ndim = ", ndim, len(start))
+    print("Nchains = ", Nchains)
+
+    for jj in range(0, Nchains):
+        f = open("%s/RSDfit_chain_COMPnbar_%d_%d_%d_%d_%d_%d_%s_%d_chain%d.dat" % (outpath, minbin1/binsize, maxbin1/binsize, minbin2/binsize, maxbin2/binsize, minbin3/binsize, maxbin3/binsize, tag, rank, jj) , "w")
+        f.close()
+        pos.append([free_ml + temperature*(2.*np.random.random_sample((ndim,))-1.)*start for i in range(nwalkers)])
+        sampler.append(emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(x, y, y_SGC, binrange1/binsize, binrange2/binsize, binrange3/binsize, maxbin1/binsize, Cinv, Cinv_SGC, free_para, fix_para)))
+        #print("start pos ",pos[jj])
+
+    # Start MCMC
+    print("Running MCMC...")
+
+    withinchainvar = np.zeros((Nchains,ndim))
+    meanchain = np.zeros((Nchains,ndim))
+    scalereduction = np.arange(ndim,dtype=np.float)
+    for jj in range(0, ndim):
+        scalereduction[jj] = 2.
+
+    itercounter = 0
+    chainstep = minlength
+    loopcriteria = 1
+    while loopcriteria:
+        
+        itercounter = itercounter + chainstep
+        print("chain length =",itercounter," minlength =",minlength)
+        
+        for jj in range(0, Nchains):
+            # Since we write the chain to a file we could put storechain=False, but in that case
+            # the function sampler.get_autocorr_time() below will give an error
+            for result in sampler[jj].sample(pos[jj], iterations=chainstep, rstate0=rstate, storechain=True, thin=ithin):
+                pos[jj] = result[0]
+                chainchi2 = -2.*result[1]
+                rstate = result[2]
+                out = open("%s/RSDfit_chain_COMPnbar_%d_%d_%d_%d_%d_%d_%s_%d_chain%d.dat" % (outpath, minbin1/binsize, maxbin1/binsize, minbin2/binsize, maxbin2/binsize, minbin3/binsize, maxbin3/binsize, tag, rank, jj), "a")
+                for k in range(pos[jj].shape[0]):
+                    out.write("{0:4d} {1:s} {2:0.6f}\n".format(k, " ".join(map(str,pos[jj][k])), chainchi2[k]))
+                out.close()
+    
+            # we do the convergence test on the second half of the current chain (itercounter/2)
+            chainsamples = sampler[jj].chain[:, itercounter/2:, :].reshape((-1, ndim))
+            #print("len chain = ", chainsamples.shape)
+            withinchainvar[jj] = np.var(chainsamples, axis=0)
+            meanchain[jj] = np.mean(chainsamples, axis=0)
+    
+        scalereduction = gelman_rubin_convergence(withinchainvar, meanchain, itercounter/2, Nchains, ndim)
+        print("scalereduction = ", scalereduction)
+        
+        loopcriteria = 0
+        for jj in range(0, ndim):
+            if np.absolute(1-scalereduction[jj]) > epsilon:
+                loopcriteria = 1
+
+        chainstep = ichaincheck
+
+    print("Done.")
 
 
 if __name__=="__main__":
--- nongausslike/model.py
+++ nongausslike/model.py
@@ -4,11 +4,11 @@ import ctypes
 import numpy as np
 
 
-lib = ctypes.CDLL("Ccode/library_combined_win_local.so")
+lib_z1 = ctypes.CDLL("Ccode/library_combined_win_local_z1.so")
     
 
-def taruya_model_module_combined_win_local(mubins, binrange1, binrange2, binrange3, maxbin1, x, alpha_perp, alpha_para, fsig8, b1NGCsig8, b1SGCsig8, b2NGCsig8, b2SGCsig8, NNGC, NSGC, sigmavNGC, sigmavSGC):
-    ''' Python wrapper for Taruya model in 
+def taruya_model(mubins, binrange1, binrange2, binrange3, maxbin1, x, alpha_perp, alpha_para, fsig8, b1NGCsig8, b1SGCsig8, b2NGCsig8, b2SGCsig8, NNGC, NSGC, sigmavNGC, sigmavSGC):
+    ''' Python wrapper for Taruya model in Florian's paper 
     '''
     x_arr = (ctypes.c_double * len(x))(*x)
     lib.taruya_model_combined_win_local.restype = ctypes.POINTER(ctypes.c_double * len(x)*2)
@@ -16,7 +16,8 @@ def taruya_model_module_combined_win_local(mubins, binrange1, binrange2, binrang
     result = lib.taruya_model_combined_win_local(ctypes.c_int(mubins),
             ctypes.c_int(binrange1),ctypes.c_int(binrange2),ctypes.c_int(binrange3), ctypes.c_int(maxbin1),
             x_arr,ctypes.c_double(alpha_perp),ctypes.c_double(alpha_para),
-            ctypes.c_double(fsig8),ctypes.c_double(b1NGCsig8),ctypes.c_double(b1SGCsig8),ctypes.c_double(b2NGCsig8),ctypes.c_double(b2SGCsig8),ctypes.c_double(NNGC), ctypes.c_double(NSGC),ctypes.c_double(sigmavNGC),ctypes.c_double(sigmavSGC))
+            ctypes.c_double(fsig8),ctypes.c_double(b1NGCsig8),ctypes.c_double(b1SGCsig8),ctypes.c_double(b2NGCsig8),ctypes.c_double(b2SGCsig8),
+            ctypes.c_double(NNGC), ctypes.c_double(NSGC),ctypes.c_double(sigmavNGC),ctypes.c_double(sigmavSGC))
     model = [i for i in result.contents]
     model = np.array(model)
     return model
